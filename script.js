bfec588c755674584dfea0a98502390f934b8449
Chris Mikhail <samuelmikhc@gmail.com>
2023-02-20 18:41:48 -0500

    added new text to header on landing page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typed", {
-  strings: ["Hi, I'm Chris Mikhail.", "I'm a CS student.", "I'm a developer.", "I'm a problem solver."],
+  strings: ["Hi, I'm Chris Mikhail.", "I'm a CS student.", "I'm a developer.", "I'm a problem solver.", "(and a problem starter)."],
   typeSpeed: 80,
   loop: true,
   backSpeed: 50,
