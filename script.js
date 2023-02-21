4940bc2c99564ca0b2bbd21362cd75f34cf1bcc9
Chris Mikhail <samuelmikhc@gmail.com>
2023-02-21 10:38:31 -0500

    changes to landing page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var typed = new Typed(".typed", {
-  strings: ["Hi, I'm Chris Mikhail.", "I'm a CS student.", "I'm a developer.", "I'm a problem solver.", "(and a problem starter)."],
+  strings: ["Hi, I'm Chris Mikhail.", "I'm a CS student.", "I'm a developer.", "I'm a problem solver.", "And a problem starter &#128553;."],
   typeSpeed: 80,
   loop: true,
   backSpeed: 50,
+  backDelay: 500,
+  startDelay: 150,
 });
